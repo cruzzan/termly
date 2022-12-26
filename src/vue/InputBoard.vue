4d5d75a9445176dd77e1e3beaf7d45251aa1c1b0
Sebastian Hultstrand <sebastian@storm-net.se>
2022-12-26 13:58:34 +0100

    Adds some logic to handle input

--- src/vue/InputBoard.vue
+++ src/vue/InputBoard.vue
@@ -1,31 +1,49 @@
 <template>
   <div class="d-flex flex-nowrap flex-row justify-content-center mt-2">
-    <button type="button" class="btn btn-light letter" @click="del">del</button>
-    <button type="button" class="btn btn-light letter">a</button>
-    <button type="button" class="btn btn-light letter">s</button>
-    <button type="button" class="btn btn-light letter">d</button>
-    <button type="button" class="btn btn-light letter">f</button>
-    <button type="button" class="btn btn-light letter">g</button>
-    <button type="button" class="btn btn-light letter">h</button>
-    <button type="button" class="btn btn-light letter">j</button>
-    <button type="button" class="btn btn-light letter">k</button>
-    <button type="button" class="btn btn-light letter">l</button>
-    <button type="button" class="btn btn-light letter">ö</button>
-    <button type="button" class="btn btn-light letter">ä</button>
-    <button type="button" class="btn btn-light letter" @click="guess">guess</button>
+    <button type="button" class="btn btn-danger letter" @click="del">del</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('a')">a</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('s')">s</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('d')">d</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('f')">f</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('g')">g</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('h')">h</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('j')">j</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('k')">k</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('l')">l</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('ö')">ö</button>
+    <button type="button" class="btn btn-light letter" @click="addLetter('ä')">ä</button>
+    <button type="button" class="btn btn-primary letter" @click="sendGuess">send</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "InputBoard",
-  methods: {
-    del(event) {
-      console.log('Ohh so you want to delete stuff?')
-    },
-    guess(event) {
-      console.log('Submit guess')
+  data() {
+    return {
+      guess: '',
     }
+  },
+  methods: {
+    del() {
+      console.log('Ohh so you want to delete stuff?')
+      this.guess = this.guess.slice(0, -1)
+      console.log(this.guess)
+    },
+    sendGuess() {
+      console.log('Submit guess')
+      if (this.guess.length > 4) {
+        console.log('Long enough to send!!')
+        this.$emit('game.guess', this.guess)
+      }
+    },
+    addLetter(letter) {
+      console.log('Adding', letter)
+      if (this.guess.length <= 10) {
+        this.guess = this.guess + letter
+      }
+      console.log(this.guess)
+    },
   }
 };
 </script>
